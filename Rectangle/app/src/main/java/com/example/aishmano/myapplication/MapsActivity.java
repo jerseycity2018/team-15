960fe15f8af27bdc043d97a7e362863f7e6d1c3b
Aishwarya Manoharan <aishmano@Aishwaryas-MacBook-Pro.local>
2018-10-26 23:16:40 -0400

    adding map to app

---
+++ Rectangle/app/src/main/java/com/example/aishmano/myapplication/MapsActivity.java
@@ -0,0 +1,36 @@
+package com.example.aishmano.myapplication;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng centralPark = new LatLng(40.780937, -73.968712);
+        mMap.addMarker(new MarkerOptions().position(centralPark).title("Marker in Central Park"));
+        float zoom = 15.0f;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centralPark, zoom));
+    }
+}
