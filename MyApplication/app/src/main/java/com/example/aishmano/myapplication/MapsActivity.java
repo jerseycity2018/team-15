6032d27f0277c0fe0868519ce19a136d41843ad2
aishwmano <aishw.mano@gmail.com>
2018-10-27 02:10:39 -0400

    commiting heat

--- MyApplication/app/src/main/java/com/example/aishmano/myapplication/MapsActivity.java
+++ MyApplication/app/src/main/java/com/example/aishmano/myapplication/MapsActivity.java
@@ -10,6 +10,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -32,5 +36,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(centralPark).title("Marker in Central Park"));
         float zoom = 15.0f;
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centralPark, zoom));
+        List<LatLng> list = null;
+
+    }
+
+    private ArrayList<LatLng> readItems(int resources) {
+        final ArrayList<LatLng> temp = new ArrayList<LatLng>();
+        InputStream inputStream = getResources().openRawResource(R.raw.test);
+        return null;
     }
 }
