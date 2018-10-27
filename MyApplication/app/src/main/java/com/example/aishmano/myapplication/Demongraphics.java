8ebf677d4b8e065d9c4ce2a71abddc63116ece55
Beatrix Tran <btrix.tran@gmail.com>
2018-10-27 02:45:21 -0400

    demographics

--- MyApplication/app/src/main/java/com/example/aishmano/myapplication/Demongraphics.java
+++ MyApplication/app/src/main/java/com/example/aishmano/myapplication/Demongraphics.java
@@ -2,8 +2,6 @@ package com.example.aishmano.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
